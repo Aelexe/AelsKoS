5617da04947636889a7754e96978ae0e241a5caf
Aelexe <Aelexe@gmail.com>
2018-07-27 14:15:44 +1200

    Fix auto attack duplicate record bug

--- modules/CombatParser.lua
+++ modules/CombatParser.lua
@@ -1,5 +1,9 @@
 AelsKoSCombatParser = AelsKoS:NewModule("CombatParser", "AceEvent-3.0")
 
+-- Tables used to record latest kills to prevent duplicate kill events from autoattacks coinciding with spells.
+AelsKoSCombatParser.kills = {}
+AelsKoSCombatParser.deaths = {}
+
 -- Masks used to determine what a source or destination object is.
 local meMask = bit.bor(COMBATLOG_OBJECT_TYPE_PLAYER, COMBATLOG_OBJECT_CONTROL_PLAYER, COMBATLOG_OBJECT_REACTION_FRIENDLY, COMBATLOG_OBJECT_AFFILIATION_MINE)
 local myPetMask = bit.bor(COMBATLOG_OBJECT_AFFILIATION_MINE, COMBATLOG_OBJECT_REACTION_FRIENDLY, COMBATLOG_OBJECT_CONTROL_PLAYER, COMBATLOG_OBJECT_TYPE_OBJECT, COMBATLOG_OBJECT_TYPE_GUARDIAN, COMBATLOG_OBJECT_TYPE_PET)
@@ -43,15 +47,33 @@ function AelsKoSCombatParser:COMBAT_LOG_EVENT_UNFILTERED()
   -- If it was our pet doing the killing.
   if(CombatLog_Object_IsA(sourceFlags, myPetMask) or CombatLog_Object_IsA(sourceFlags, meMask)) then
     -- And if it was a player killed.
-    if(CombatLog_Object_IsA(destFlags, enemyPlayerMask)) then
+    if(CombatLog_Object_IsA(destFlags, enemyPlayerMask) and self:notDuplicateKill(destGUID)) then
       AelsKoS:addKill(destGUID, time(), C_Map.GetBestMapForUnit("player"), spellId)
     end
   elseif (destGUID == UnitGUID("player")) then -- Otherwise, if it was us doing the dying.
     -- If a player killed us.
-    if(CombatLog_Object_IsA(sourceFlags, enemyPlayerMask)) then
+    if(CombatLog_Object_IsA(sourceFlags, enemyPlayerMask) and self:notDuplicateDeath(sourceGUID)) then
       AelsKoS:addDeath(sourceGUID, time(), C_Map.GetBestMapForUnit("player"), spellId)
     end
     -- Ideally I'd capture pet kills too but there's literally no way to do that.
+  end
+end
+
+function AelsKoSCombatParser:notDuplicateKill(guid)
+  if self.kills[guid] == nil or time() ~= self.kills[guid] then
+    self.kills[guid] = time
+    return true
+  else
+    return false
+  end
+end
+
+function AelsKoSCombatParser:notDuplicateDeath(guid)
+  if self.deaths[guid] == nil or time() ~= self.deaths[guid] then
+    self.deaths[guid] = time
+    return true
+  else
+    return false
   end
 end
 
